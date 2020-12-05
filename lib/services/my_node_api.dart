93a36757ff18a6ce98cb9c9101ac508eb540d88e
Tomas Baran <tomas.baran@gmail.com>
2020-12-05 21:12:07 +0000

    send email to my backend

---
+++ lib/services/my_node_api.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+
+class MyNodeApi {
+  // final String _url = 'localhost:3000';
+  // final String _path = '/subscribe';
+
+  final String _url = 'us-central1-ambee-cloud.cloudfunctions.net';
+  final String _path = '/app/subscribe';
+
+  addSubscriber(String email) async {
+    Map _body = {'email': email};
+    http.Response _response = await http.post(Uri.https(_url, _path), /* headers: _headers ,*/ body: _body);
+
+    if (_response.statusCode != 200 || _response.statusCode != 201)
+      throw 'Error #1 (coming from MailerLite server): \n\t status code: ${_response.statusCode} \n\t status message: ${_response.reasonPhrase}';
+    print(_response.body);
+  }
+}
